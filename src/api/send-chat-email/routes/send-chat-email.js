2263342a95802664f923a19fa0fde2a714fba143
waqas837 <waqaskhanbughlani1124@gmail.com>
2024-12-23 10:32:20 +0500

    chat features are updated

---
+++ src/api/send-chat-email/routes/send-chat-email.js
@@ -0,0 +1,13 @@
+module.exports = {
+  routes: [
+    // {
+    //  method: 'GET',
+    //  path: '/send-chat-email',
+    //  handler: 'send-chat-email.exampleAction',
+    //  config: {
+    //    policies: [],
+    //    middlewares: [],
+    //  },
+    // },
+  ],
+};
---
+++ src/api/send-chat-email/services/send-chat-email.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * send-chat-email service
+ */
+
+module.exports = () => ({});
